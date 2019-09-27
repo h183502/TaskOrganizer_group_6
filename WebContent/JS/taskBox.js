635eab7ded51fede14432ffd5130dc2834e1ce27
jhoffis <jonahoffis@gmail.com>
2019-09-27 22:38:38 +0200

    Fixed ToolBox.js and cleaned up index.html

---
+++ WebContent/JS/taskBox.js
@@ -0,0 +1,90 @@
+"use strict";
+
+class taskBox {
+
+    constructor(modaldiv) {
+        this.modal = modaldiv
+
+        //Lag en array med attributes title og status og send den inn i submitTask
+        this.button =  $(this.modal).find('button')[0]
+        this.statusElem = $(this.modal).find('select#status')
+        this.titleElem = $(this.modal).find('input#title')
+    }
+
+    set allStatuses(s) {
+        this.statuses = s
+        this.statuses.forEach(status => {
+            let opt = new Option(status, status);
+            this.statusElem[0].options.add(opt)
+        });
+    }
+
+    set onSubmit(newtask) {
+        this.submitTask = newtask;
+    }
+
+    close() {
+        console.log("CLOSE TASKBOX");
+        this.modal.style.display = "none";
+
+        //Reset verdier
+        this.statuses.selectedIndex = 0
+        this.titleElem[0].value = ""
+    }
+
+    show() {
+        console.log("SHOW TASKBOX");
+        this.modal.style.display = "block";
+    }
+
+    isVisible() {
+        return this.modal.style.display === "block";
+    }
+
+
+}
+
+const modalDiv = document.getElementsByClassName("modal")[0];
+const newTaskButton = document.getElementById("newTask");
+
+let taskBoxInstance = new taskBox(modalDiv);
+let lastClick = 0;
+
+window.window.addEventListener("click", function (event) {
+    console.log("CLICKED ON WINDOW - taskBox REGISTERED")
+    if (taskBoxInstance.isVisible()
+        && lastClick === 0 &&
+        !document.querySelector('.modal-content').contains(event.target)) {
+
+        taskBoxInstance.close();
+    }
+    lastClick = 0;
+});
+
+newTaskButton.addEventListener("click", (event) => {
+    taskBoxInstance.show()
+    lastClick = 1;
+}, true);
+
+let closeBtn = document.getElementsByClassName("close")[0];
+closeBtn.addEventListener("click", function (event) {
+    taskBoxInstance.close();
+});
+
+taskBoxInstance.allStatuses = statuses;
+
+taskBoxInstance.onSubmit = (task) => {
+    console.log(`New task '${task.title}' with initial status ${task.status} is added by the user.`)
+    gui.showTask(task)
+    taskBoxInstance.close()
+};
+
+taskBoxInstance.button.addEventListener("click", function (event) {
+    let task = {
+        "id": Math.random() * 1000000,
+        "title": taskBoxInstance.titleElem.val(),
+        "status": taskBoxInstance.statusElem.val()
+    };
+
+    taskBoxInstance.submitTask(task);
+});
